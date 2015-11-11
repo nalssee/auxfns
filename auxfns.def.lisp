259869e813160a33a5e3ad83d0ef7b9e28b2bd07
nalssee <kenjin@sdf.org>
2015-11-11 14:51:51 +0900

    revert back auxfns.def

--- auxfns.def.lisp
+++ auxfns.def.lisp
@@ -4,160 +4,83 @@
 
 (in-package :auxfns.def)
 
-(defmacro def (&body exps)
+;; 05/28/2014
+;; Syntactic sugar for pattern matching based definition
+;; See the below of this file for example
+
+(defmacro def (&body sexps)
   `(progn ,@(mapcar
 	     ;; form defparameter or defun
-	     #'form-definition
-	     (group (group exps :n 2)
-		    :key #'clause-name))))
-
-;; Data abstractions
-(defun clause-head (clause) (first clause))
-(defun clause-body (clause) (second clause))
-(defun clause-name (clause)
-  (if (atom (clause-head clause)) ; parameter def
-      (clause-head clause)
-      (first (clause-head clause))))
-
-(defun type-spec-clause-p (clause)
-  "Tests if the given clause specifies types"
-  (and (listp (clause-head clause))
-       (<= 2 (length (clause-head clause)))
-       (eql (second (clause-head clause)) (intern "!!"))))
-
-(defun clause-parameters (clause)
-  (rest (clause-head clause)))
-
-(defun clause-parameter-types (type-spec-clause)
-  (cddr (clause-head type-spec-clause)))
-
-(defun clause-return-type (type-spec-clause)
-  (clause-body type-spec-clause))
-
-;; make a list of gensyms for paramters
-(defun make-parameters (clause)
-  (loop repeat (1- (length (clause-head clause))) collect (gensym)))
-
-(defun variable-pattern-p (x)
-  (and (atom x) (symbolp x)
-       (not (eql x t)) (not (eql x nil))))
-
-(defun variable-pattern-only-p (parameters)
-  (loop for p in parameters always (variable-pattern-p p)))
+	     #'definition
+	     (group-by-definition-name (group sexps :n 2)))))
 
 
+(defun group-by-definition-name (clauses)
+  (group clauses
+	 :key #'definition-name))
 
-;;;
-(defvar *def-dispatch-table* (make-hash-table))
+(defun definition-name (clause)
+  (if (atom (car clause))
+      (car clause)
+      (caar clause)))
 
-(defun register-def (name fn)
-  (setf (gethash name *def-dispatch-table*) fn))
-
-(defun form-definition (clauses)
-  (if (and (null (rest clauses)) (atom (clause-head (first clauses))))
-      (let ((clause (first clauses)))
-	`(defparameter ,(clause-name clause) ,(clause-body clause)))
-      `(defun ,@(funcall (gethash (analyze-clauses clauses) *def-dispatch-table*)
-			 clauses))))
-
-;; 1 clause, no type spec, no pattern variables
-(register-def
- :simple-form
- #'(lambda (clauses)
-     (let ((cl1 (first clauses)))
-       `(,(clause-name cl1) ,(clause-parameters cl1)
-	  ,(handle-local-definitions (clause-body cl1))))))
-
-;; requires match form (multiple clauses or pattern variable)
-;; no type spec 
-(register-def
- :match-form
- #'(lambda (clauses)
-     (let* ((cl1 (first clauses))
-	    (params (make-parameters cl1)))
-       `(,(clause-name cl1) ,params
-	  (match (list ,@params)
-	    ,@(loop for (pattern value) in clauses collect
-		   (list `(list ,@(rest pattern)) (handle-local-definitions value))))))))
-
-;; 1 body clause, type spec, no pattern variables
-(register-def
- :simple-form-type
- #'(lambda (clauses)
-     (destructuring-bind (tsc cl1) clauses
-       `(,(clause-name cl1) ,(clause-parameters cl1)
-	  ,(form-declaration tsc (clause-parameters cl1))
-	  (the ,(clause-return-type tsc) ,(handle-local-definitions
-				    (clause-body cl1)))))))
+(defun definition (clauses)
+  (if (and (null (cdr clauses)) (atom (caar clauses)))
+      (cons 'defparameter (var-value clauses))
+      (cons 'defun (var-args-value clauses))))
 
 
-;; Hardest part
-;; requires match form with type spec
-(register-def
- :match-form-type
- #'(lambda (clauses)
-     (destructuring-bind (tsc cl1 &rest cls) clauses
-       (declare (ignore cls))
-       (let ((params (make-parameters cl1)))
-	 `(,(clause-name cl1) ,params
-	    ,(form-declaration tsc params)
-	    (match (list ,@params)
-	      ,@(loop for (pattern value) in (rest clauses) collect
-		     `((list ,@(rest pattern))
-		       ,@(maybe-form-declaration tsc (rest pattern))
-		       (the ,(clause-return-type tsc) ,(handle-local-definitions
-						 value))))))))))
+(defun var-value (clauses)
+  `(,(caar clauses) ,(cadar clauses)))
 
+(defun var-args-value (clauses)
+  (let* ((cl1 (car clauses)))
+    (if (null (cdr clauses))
+	(if (not (every #'variable-p (pattern cl1)))
+	    (error "Single clause definition must not have any constant in a pattern ~A"
+		   cl1)
+	    `(,(caar cl1) ,(cdar cl1)
+	       ,(let-labels-body (second cl1))))
+	(let ((params (params cl1)))
+	  `(,(caar cl1) ,params
+	     ,(function-body-for-multiple-clauses clauses params))))))
 
-(defun handle-local-definitions (exp)
-  (if (and (listp exp) (eql (first exp) (intern "WITH-LOCAL-DEFINITIONS")))
-      (let* ((clauses (group (second exp) :n 2))
-	     ;; todo:
-	     ;; Maybe I should restrict let bindings come before labels bindings
-	     (let-clauses (remove-if
-			   #'(lambda (c) (listp (clause-head c))) clauses))
-	     (labels-clauses (remove-if-not
-			      #'(lambda (c) (listp (clause-head c))) clauses))
-	     (labels-form
-	      `(labels
-		   ,(loop for cls in (group labels-clauses :key #'clause-name) collect
-			 (funcall (gethash (analyze-clauses cls) *def-dispatch-table*) cls))
-		 ,@(cddr exp))))
-	;; At least one labels-bindings exists	
-	;; 	(assert (not (null labels-clauses)))
-	(if (null let-clauses)
-	    labels-form
-	    `(let ,let-clauses
-	       ,labels-form)))
-      exp))
+(defun function-body-for-multiple-clauses (clauses params)
+  `(match (list ,@params)
+     ,@(mapcar #'(lambda (c)
+		   (list (cons 'list (pattern c))
+			 (let-labels-body (second c))))
+	       clauses)))
 
+(defun params (clause)
+  (let ((result '()))
+    (dotimes (i (length (pattern clause)))
+      (push (gensym) result))
+    result))
 
-(defun analyze-clauses (clauses)
-  (destructuring-bind (cl1 &rest cls) clauses
-    (cond ((and (null cls)
-		(variable-pattern-only-p (clause-parameters cl1)))
-	   :simple-form)
-	  ;; todo, error case
-	  ;; spec clause only definition may need to be handled as well
-	  ((type-spec-clause-p cl1)
-	   (if (and (null (rest cls))
-		    (variable-pattern-only-p (clause-parameters (first cls))))
-	       :simple-form-type
-	       :match-form-type))
-	  (t :match-form))))
+(defun variable-p (x)
+  (and (symbolp x) (not (eq x nil)) (not (eq x t))))
 
-(defun form-declaration (type-spec-clause parameters)
-  `(declare (optimize (speed 3) (safety 0))
-	    ,@(loop for type in (clause-parameter-types type-spec-clause)
-		   for param in parameters collect
-		   `(type ,type ,param))))
+(defun pattern (clause) (cdar clause))
 
-(defun maybe-form-declaration (type-spec-clause parameters)
-  (let ((types (loop for type in (clause-parameter-types type-spec-clause)
-		  for param in parameters when (variable-pattern-p param)
-		  collect `(type ,type ,param))))
-    (when types
-      `((declare ,@types)))))
+(defun let-labels-body (clause-value)
+  (if (contains-where-p clause-value)
+      (let* ((internal-clauses (group (cddr clause-value) :n 2))
+	     ;; let binds come before labels binds automatically.
+	     (let-binds (remove-if #'function-clause-p internal-clauses))
+	     (labels-binds (remove-if-not #'function-clause-p internal-clauses)))
+	(if (null let-binds)
+	    `(labels ,(mapcar #'var-args-value (group-by-definition-name labels-binds))
+	       ,(car clause-value))
+	    `(let ,let-binds
+	       (labels ,(mapcar #'var-args-value (group-by-definition-name labels-binds))
+		 ,(car clause-value)))))
+      clause-value))
 
+(defun contains-where-p (clause-value)
+  (and (consp clause-value)
+       (consp (first clause-value))
+       (find (intern "WHERE") clause-value)))
 
-
+(defun function-clause-p (clause)
+  (consp (car clause)))
--- test/def_test.lisp
+++ test/def_test.lisp
@@ -8,14 +8,12 @@
 
 (def
   e 2.718
-  
-  (fib1 !! fixnum) fixnum
+
   (fib1 0) 0
   (fib1 1) 1
   (fib1 n) (+ (fib1 (1- n)) (fib1 (- n 2)))
 
   ;; This is faster
-  (fib2 !! fixnum) fixnum
   (fib2 n) (cond ((= n 0) 0)
   		 ((= n 1) 1)
   		 (t (+ (fib2 (- n 1)) (fib2 (- n 2)))))
@@ -30,14 +28,15 @@
   (my-append nil y) y
   (my-append (cons x y) z) (cons x (my-append y z))
 
-  (sqrt1 x) (with-local-definitions
-		((good-enough? guess) (< (abs (- (* guess guess) x)) 0.001)
-		 (improve guess) (average guess (/ x guess))
-		 (average a b) (/ (+ a b) 2.0)
-		 (sqrt-iter guess) (cond ((good-enough? guess) guess)
-					 (t (sqrt-iter (improve guess)))))
-	      (sqrt-iter 1.0))
-  
+  (sqrt1 x) ((sqrt-iter 1.0)
+  	     where
+  	     (good-enough? guess) (< (abs (- (* guess guess) x)) 0.001)
+	     (improve guess) (average guess (/ x guess))
+	     (average a b) (/ (+ a b) 2.0)
+	     (sqrt-iter guess) (cond ((good-enough? guess) guess)
+				     (t (sqrt-iter (improve guess)))))
+
+
   (capital-city 'Korea) 'Seoul
   (capital-city 'China) 'Beijing
   (capital-city 'Japan) 'Tokyo
@@ -55,5 +54,3 @@
   (assert-true (equal (capital-city 'Brasil) 'no-idea)))
 
 (print (run-suite 'simple-test-suite))
-
-
